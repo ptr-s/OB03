7af01b962453679c791e1772ec8e98864476592a
ptr-s <rmsptr@yahoo.com>
2024-12-15 21:17:04 +0300

    update class tree

--- zoo.py
+++ zoo.py
@@ -26,18 +26,51 @@
 #=========================================
 # Animals
 
+class Animals:
+    def __init__(self, animals_list: []):
+        self.__items = []
+        self.__animals = animals_list
+
+    def show(self):
+        print(f"Животных в зоопарке: {len(self.__items)}")
+        for i, animal in enumerate(self.__items):
+            print(f"{i}. ", end='')
+            animal.info()
+
+    def accept(self, **animal):
+        pass
+
+    def release(self, animal_number: int):
+        if 0 <= animal_number < len(self.__items):
+            message = f"Животное '{self.__items[animal_number].name}' - отпущено"
+            del self.__items[animal_number]
+            print(message)
+        else:
+            print(f"Животное c №{animal_number} не найдено")
+
+
 class Animal:
-    def __init__(self, name, age, voice, food):
-        self.name = name
+    __biology_class = None
+
+    def __init__(self, species, nickname, age, voice, food):
+        self.species = species
+        self.nickname = nickname
         self.age = age
         self.voice = voice
         self.food = food
 
+    @staticmethod
+    def get_biology_class():
+        return Animal.__biology_class
+
+    def get_my_biology_class(self):
+        return self.__biology_class
+
     def make_sound(self):
-        print(f"{self.name} издаёт звук: {self.voice}")
+        print(f"{self.species} '{self.nickname}' {self.voice}")
 
     def eat(self):
-        print(f"{self.name} ест {self.food}")
+        print(f"{self.species} '{self.nickname}' ест {self.food}")
 
     def make_move(self):
         pass
@@ -52,39 +85,98 @@ class Animal:
         pass
 
 class Bird(Animal):
-    def __init__(self, name, age, voice, food, wingspan):
-        super().__init__(name, age, voice, food)
+    __biology_class = "Птицы"
+
+    def __init__(self, species, nickname, age, voice, food, wingspan):
+        super().__init__(species, nickname, age, voice, food)
         self.wingspan = wingspan
 
+    @staticmethod
+    def get_biology_class():
+        return Bird.__biology_class
+
     def make_move(self):
-        print(f"{self.name} летает по вольеру")
+        print(f"{self.species} '{self.nickname}' летает по вольеру")
 
 class Mammal(Animal):
-    def __init__(self, name, age, voice, food, weight):
-        super().__init__(name, age, voice, food)
+    __biology_class = "Млекопитающие"
+
+    def __init__(self, species, nickname, age, voice, food, weight):
+        super().__init__(species, nickname, age, voice, food)
         self.weight = weight
 
+    @staticmethod
+    def get_biology_class():
+        return Mammal.__biology_class
+
     def make_move(self):
-        print(f"{self.name} ходит по вольеру")
+        print(f"{self.species} '{self.nickname}' ходит по вольеру")
 
 class Reptile(Animal):
-    def __init__(self, name, age, voice, food, length):
-        super().__init__(name, age, voice, food)
+    __biology_class = "Пресмыкающиеся"
+
+    def __init__(self, species, nickname, age, voice, food, length):
+        super().__init__(species, nickname, age, voice, food)
         self.length = length
 
+    @staticmethod
+    def get_biology_class():
+        return Reptile.__biology_class
+
     def make_move(self):
-        print(f"{self.name} ползает по террариуму")
+        print(f"{self.species} '{self.nickname}' ползает по террариуму")
 
 #=========================================
-# Employees
+# Staff
+
+class Staff:
+    def __init__(self, job_list: []):
+        self.__items = []
+        self.__jobs = job_list
+
+    def show(self):
+        print(f"Сотрудников в зоопарке: {len(self.__items)}")
+        for i, employee in enumerate(self.__items):
+            print(f"{i}. ", end='')
+            employee.info()
+
+    def hire(self, **employee):
+        job_class = employee['job_class']
+        job_title = employee['job_title']
+        name = employee['name']
+        # if job_class and job_class in self.__jobs:
+        #     print("")
+        # elif job_title and in [for item in ]
+        # else:
+        #     print("")
+
+    def fire(self, employee_number: int):
+        if 0 <= employee_number < len(self.__items):
+            message = f"Сотрудник {self.__items[employee_number].name} - сокращён"
+            del self.__items[employee_number]
+            print(message)
+        else:
+            print(f"Сотрудника с №{employee_number} не найдено")
+
+    def load(self, data: {}):
+        pass
+
+    def save(self):
+        pass
+
 
 class Employee:
-    def __init__(self, name, job_title):
+    __job_title = None
+
+    def __init__(self, name):
         self.name = name
-        self.job_title = job_title
+
+    @staticmethod
+    def get_job_title():
+        return Employee.__job_title
 
     def info(self):
-        print(f"{self.name} - {self.job_title}")
+        print(f"{self.name} - {self.get_job_title}")
 
     def action(self, animal):
         pass
@@ -96,62 +188,38 @@ class Employee:
         pass
 
 class ZooKeeper(Employee):
+    __job_title = "Смотритель зоопарка"
+
     def __init__(self, name):
-        super().__init__(name, "Смотритель зоопарка")
+        super().__init__(name)
+
+    @staticmethod
+    def get_job_title():
+        return ZooKeeper.__job_title
 
     def action(self, animal: Animal):
-        print(f"{self.job_title} {self.name} кормит {animal.name}")
+        print(f"{self.get_job_title} {self.name} кормит животное: {animal.species} '{animal.nickname}'")
 
 class Veterinarian(Employee):
+    __job_title = "Ветеринар"
+
     def __init__(self, name):
-        super().__init__(name, "Ветеринар")
+        super().__init__(name)
+
+    @staticmethod
+    def get_job_title():
+        return Veterinarian.__job_title
 
     def action(self, animal: Animal):
-        print(f"{self.job_title} {self.name} лечит {animal.name}")
+        print(f"{self.get_job_title} {self.name} лечит  животное: {animal.species} '{animal.nickname}'")
 
 #=========================================
 # Zoo
 
 class Zoo:
     def __init__(self):
-        self.employees = []
-        self.animals = []
-
-    def show_employees(self):
-        print(f"Сотрудников в зоопарке: {len(self.employees)}")
-        for i, employee in enumerate(self.employees):
-            print(f"{i}. ", end='')
-            employee.info()
-
-    def hire(self, employee: {}):
-        pass
-
-    def fire(self, employee_number: int):
-        employee_number = int(employee_number)
-        if 0 <= employee_number < len(self.employees):
-            message = f"Сотрудник {self.employees[employee_number].name} - сокращён"
-            del self.employees[employee_number]
-            print(message)
-        else:
-            print(f"Сотрудника с №{employee_number} не найдено")
-
-    def show_animals(self):
-        print(f"Животных в зоопарке: {len(self.animals)}")
-        for i, animal in enumerate(self.employees):
-            print(f"{i}. ", end='')
-            animal.info()
-
-    def accept(self, animal: {}):
-        pass
-
-    def release(self, animal_number: int):
-        animal_number = int(animal_number)
-        if 0 <= animal_number < len(self.animals):
-            message = f"Животное '{self.animals[animal_number].name}' - отпущено"
-            del self.animals[animal_number]
-            print(message)
-        else:
-            print(f"Животное c №{animal_number} не найдено")
+        self.staff = Staff([ZooKeeper, Veterinarian])
+        self.animals = Animals([Bird, Mammal, Reptile])
 
     def load(self, file_name: str):
         pass
@@ -159,20 +227,29 @@ class Zoo:
     def save(self, file_name: str):
         pass
 
+#=========================================
+
 FILE_NAME = "zoo.json"
 
 def main():
     zoo = Zoo()
     zoo.load("FILE_NAME")
     try:
-        if not zoo.employees and not zoo.animals:
+        if not zoo.staff and not zoo.animals:
             # Значение по умолчанию
-            zoo.hire({"name"})
-            pass
+            # Сотрудники
+            zoo.staff.hire(job_class=ZooKeeper, name="Семён Семёныч")
+            zoo.staff.hire(job_class=Veterinarian, name="Наталья Юрьевна")
+            # Животные
+            zoo.animals.accept(animal_class=Bird, species="Филин", nickname="Соня", age=27, voice="ухает", food="мышь", wingspan=1.3)
+            zoo.animals.accept(animal_class=Mammal, species="Бурый медведь", nickname="Потапыч", age=21, voice="рычит", food="мясо", weight=250)
+            zoo.animals.accept(animal_class=Mammal, species="Белый медведь", nickname="Умка", age=18, voice="рычит", food="мясо", weight="400")
+            zoo.animals.accept(animal_class=Reptile, species="Королевская кобра", nickname="Наг", age=27, voice="шипит", food="мышь", length=3.5)
 
-        zoo.show_employees()
+        zoo.staff.show()
         print(" ")
-        zoo.show_animals()
+        zoo.animals.show()
+        """
         while True:
             print("\nУправление зоопарком."
                   f"Сотрудников в зоопарке: {len(zoo.employees)}"
@@ -208,6 +285,7 @@ def main():
 
             except ValueError:
                 print("Ошибка: Введите числовое значение.")
+             """
     finally:
         zoo.save("FILE_NAME")
         pass
